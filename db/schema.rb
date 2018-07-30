14230cf5dd6cd457f909ba9815fb6836a19f59d0
Howard DeVennish <howarddevennish@gmail.com>
2018-07-30 20:05:00 +0000

    add user and user_game models and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730195614) do
+ActiveRecord::Schema.define(version: 20180730200417) do
+
+  create_table "games", force: :cascade do |t|
+    t.string   "title"
+    t.string   "console"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_games", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
